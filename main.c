62a5cd1797c7b070d2011f608bb49dda920aba70
Grace Lavin <gcl5087@psu.edu>
2020-09-01 14:09:29 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+  //Author: Grace Lavin gcl5087@psu.edu
+// Collaborator: Justin Tien - jst5381@psu.edu
+// Collaborator: Henry Radzikowksi - har5254@psu.edu
+// Collaborator: Ruilan Sun - rfs5748@psu.edu
+
+#include <stdio.h>
+#include <readline/readline.h> 
+#include <stdlib.h>
+
+int main(void) {
+
+ char *celsiusV = readline("Enter temperature in celsius: ");
+
+  double doubleCel = atof(celsiusV);
+
+  double fahrV = (1.8* (doubleCel) + 32);
+
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.", doubleCel, fahrV);
+  return 0;
+}
